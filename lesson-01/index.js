35d1718dcfe02088249fb192d9cc7a69fcf4304b
Andrew Van Slaars <avanslaars@gmail.com>
2017-12-17 16:01:06 -0500

    Initial setup

---
+++ lesson-01/index.js
@@ -0,0 +1,6 @@
+const { dbl, inc } = require('../utils')
+
+const input = 2
+const result = inc(input)
+
+console.log(result)
---
+++ utils.js
@@ -0,0 +1,18 @@
+/**
+ * Returns the result of incrementing the supplied number by 1
+ * @param {number} n
+ * @returns {number}
+ */
+const inc = n => n + 1
+
+/**
+ * Returns the result of doubling the supplied number
+ * @param {number} n
+ * @returns {number}
+ */
+const dbl = n => n * 2
+
+module.exports = {
+  dbl,
+  inc
+}
