5e80bd38fdf7bb70901d9599509ac5d4ad9d4e6c
Andrew Van Slaars <avanslaars@gmail.com>
2017-12-18 08:28:46 -0500

    Completed lesson 01

--- lesson-01/index.js
+++ lesson-01/index.js
@@ -1,6 +1,12 @@
-const { dbl, inc } = require('../utils')
+const { inc } = require('../utils')
+const Maybe = require('crocks/Maybe')
 
-const input = 2
-const result = inc(input)
+// Maybe = Just x | Nothing
+
+const safeNum = val =>
+  typeof val === 'number' ? Maybe.Just(val) : Maybe.Nothing()
+
+const input = safeNum(2)
+const result = input.map(n => console.log('calling inc') || inc(n))
 
 console.log(result)
