7f02c8f1b70cc7c2135b2761814e24a028ebe3e9
Andrew Van Slaars <avanslaars@gmail.com>
2017-12-24 11:35:53 -0500

    Updated index for lesson 3

--- lesson-03/index.js
+++ lesson-03/index.js
@@ -1,11 +1,12 @@
 const safe = require('crocks/Maybe/safe')
 const isNumber = require('crocks/predicates/isNumber')
+const compose = require('crocks/helpers/compose')
 const { inc, dbl } = require('../utils')
 
-const incDbl = n => {
-  const incremented = inc(n)
-  return dbl(incremented)
-}
+const incDbl = n =>
+  safe(isNumber, n)
+    .map(compose(dbl, inc))
+    .option(0)
 
 const result = incDbl(2)
 console.log(result)
